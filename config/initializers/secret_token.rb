a199b089fc4c7a034d29fbc5cc28f5110cf2b74e
FelixCoutinho <felix.coutinho@gmail.com>
2011-10-14 10:56:39 -0300

    Adicionado os arquivos faltantes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracker::Application.config.secret_token = '9c3a2b3acb0bb90de63b687f39196cc5dd984f3506bc3402df400acd3598932dfdeb749d726dcbea4e1b8b47c32029b5f45b4ba1742f379c735a0bf6499ff1e4'
